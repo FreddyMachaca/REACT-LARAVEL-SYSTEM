cac03b345e25839aa10a1f48b494a33d38a215f0
Freddy Machaca <freddyymachaca@gmail.com>
2025-03-24 08:14:48 -0400

    Cambios DB

--- CambioDB.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE tbl_seg_menu
-ALTER COLUMN me_icono TYPE VARCHAR(255);
-
-ALTER TABLE tbl_seg_menu
-ALTER COLUMN me_url TYPE VARCHAR(255);
-
-ALTER TABLE public.tbl_kd_educacion_formal ALTER COLUMN ef_centro_form TYPE int4 USING ef_centro_form::int4;
---
+++ DB/CambiosDB.sql
@@ -0,0 +1,1 @@
+ALTER TABLE public.tbl_kd_educacion_formal ALTER COLUMN ef_centro_form TYPE int4 USING ef_centro_form::int4;
